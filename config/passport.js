f7809dcf25add76d9e747b25f30580ba4f8fd9aa
VahapZTL <120320108@student.beykent.edu.tr>
2017-03-30 11:23:57 +0300

    Css bud fixed, Passport Config Updated

--- config/passport.js
+++ config/passport.js
@@ -59,7 +59,7 @@ module.exports = function(passport) {
                         newUser.save(function(err) {
                             if (err)
                                 throw err;
-                            return done(null, newUser);
+                            return done(null);
                         });
                     }
                 });
--- routes/signup.js
+++ routes/signup.js
@@ -8,7 +8,7 @@ router.get('/', isLoggedIn, function(req, res, next) {
 });
 
 router.post('/', isLoggedIn, passport.authenticate('local-signup', {
-    successRedirect : '/', // redirect to the secure profile section
+    successRedirect : '/signup', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 }));
