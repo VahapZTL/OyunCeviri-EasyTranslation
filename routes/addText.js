9629658f4efde314d07267a224bca9d43604647d
Abdulvahap ÖZTEL <vahapoztel61@hotmail.com>
2018-02-02 01:42:07 +0300

    Xlsx lib was added the project
    Handled xlsx file with xlsx lib.

--- routes/addText.js
+++ routes/addText.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var mongoose = require('mongoose');
 var MainText = mongoose.model('MainText');
 var unzip = require('unzip-stream');
+var excel = require('xlsx');
 
 router.get('/', isLoggedIn, function(req, res, next) {
     res.render('addText', {user: req.user});
@@ -15,16 +16,33 @@ router.post('/', isLoggedIn, function (req, res, next) {
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     const incomeFile = req.files.mainText;
-    const fileName = req.files.mainText.name;
+    const __fileName = req.files.mainText.name;
+
+    const fileName = __fileName.replace("zip","");
 
     // Use the mv() method to place the file somewhere on your server
-    incomeFile.mv(process.cwd() + '/public/uploads/' + fileName + '.zip' , function(err) {
+    incomeFile.mv(process.cwd() + '/public/uploads/' + fileName + '.zip', function(err) {
         if (err)
             return res.status(500).send(err);
 
-        fs.createReadStream(process.cwd() + '/public/uploads/' + fileName + '.zip')
-            .pipe(unzip.Extract({ path: process.cwd() + '/public/uploads/' }));
-        res.send('File uploaded!');
+        new Promise(function(resolve,reject) {
+
+            fs.createReadStream(process.cwd() + '/public/uploads/' + fileName + '.zip')
+                .pipe(unzip.Extract({ path: process.cwd() + '/public/uploads/' }));
+            resolve(process.cwd() + '/public/uploads/' + fileName + '.xlsx');
+
+        }).then(function(result) {
+
+            var workbook = excel.readFile(result);
+            var sheet_name_list = workbook.SheetNames;
+            var excelData = excel.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+            console.log(excelData);
+
+            res.send('File uploaded!');
+            return excelData;
+        }).catch(function (err) {
+            reject(err);
+        });
     });
 });
 
